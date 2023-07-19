53569484b28c304a17202dc23d269da2128beb6c
mangjong <akdwhd0921@gmail.com>
2023-07-19 13:28:47 +0900

    Update mail_boannews.py

--- mail_boannews.py
+++ mail_boannews.py
@@ -90,7 +90,6 @@ def send_mail(new_links, to_mail):
     #Config / Mail List, basic content
     from_name = "YOUR NAME"
     from_mail = 'YOUR FROM MAIL'
-    #to_mail = 'YOUR TO MAIL'
     app_key = 'YOUR APP KEY'
 
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
